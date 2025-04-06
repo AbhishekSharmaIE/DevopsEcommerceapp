cb8a25fb49cc35d284cba59929f35a081466c76c
AbhishekSharmaIE <abhisheksharma.irl@gmail.com>
2025-04-06 22:21:12 +0100

    Update application configuration for Elastic Beanstalk port settings

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
 from app import app as application
 
 if __name__ == "__main__":
-    application.run() 
+    # This is used when running locally only. When deploying to Elastic Beanstalk,
+    # the application object itself is used by the gunicorn server
+    application.run(host='0.0.0.0', port=5000) 
